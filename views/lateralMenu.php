174b492e82ca01bd8a34d1ee68bf32dfe2158fa9
Cesar Ivan Rivera <cesarivan25479@gmail.com>
2022-07-16 12:05:29 -0500

    Menu lateral

---
+++ views/lateralMenu.php
@@ -0,0 +1,144 @@
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+    <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+    <li class="nav-item">
+        <a href="../index.html" class="nav-link active">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>
+                Dashboard
+                <i class="right fas fa-angle-left"></i>
+            </p>
+        </a>
+        <ul class="nav nav-treeview">
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Administración</p>
+                </a>
+            </li>
+        </ul>
+    </li>
+
+    <li class="nav-item menu-open">
+        <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-solid fa-headset"></i>
+            <p>
+                Soporte técnico
+                <i class="right fas fa-angle-left"></i>
+            </p>
+        </a>
+        <ul class="nav nav-treeview">
+            <li class="nav-item">
+                <a href="./Clientes.php" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Nuevo Reporte</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link active" data-toggle="modal" data-target="#IntFecha">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Reportes Pendiente</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="./OrdenesInstalacion.php" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Reportes Atendidos</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="./OrdenesInstalacion.php" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Estadisticas</p>
+                </a>
+            </li>
+        </ul>
+    </li>
+
+    <li class="nav-item">
+        <a href="../index.html" class="nav-link">
+            <i class="nav-icon fas fa-calendar"></i>
+            <p>
+                Instalaciones
+                <i class="right fas fa-angle-left"></i>
+            </p>
+        </a>
+        <ul class="nav nav-treeview">
+            <li class="nav-item">
+                <a href="./PagosBanco.php" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Nueva Instalación</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Instalaciones Pendientes</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Instalaciones Realizdas</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Instalaciones Canceladas</p>
+                </a>
+            </li>
+        </ul>
+    </li>
+
+    <li class="nav-item">
+        <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-sitemap"></i>
+            <p>
+                Sistema
+                <i class="right fas fa-angle-left"></i>
+            </p>
+        </a>
+        <ul class="nav nav-treeview">
+            <li class="nav-item">
+                <a href="./routers.php" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Router</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Corte</p>
+                    <i class="right fas fa-angle-left"></i>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="far fa-dot-circle nav-icon"></i>
+                            <p>Coyoltepec</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Puntos de Acceso</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Planes de internet</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Zonas</p>
+                </a>
+            </li>
+        </ul>
+    </li>
+</ul>
