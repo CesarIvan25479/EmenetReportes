815ce35f0726682757e6f0840b2ea6c73eaaae8d
Cesar Ivan Rivera Castro <cesarivan25479@gmail.com>
2022-11-26 17:51:08 -0600

    Conexion OLT

--- index1.php
+++ index1.php
@@ -1,424 +1,59 @@
-<!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
-<html lang="en">
+<html>
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Home</title>
-
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
-  <link rel="icon" href="./dist/img/Logosinfondo.svg">
-
+    <title>Conexion por Telent</title>
 </head>
-<body class="hold-transition sidebar-mini sidebar-collapse">
-<div class="wrapper">
 
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Reporte pagos</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="Clientes" class="nav-link">Administración</a>
-      </li>
-    </ul>
+<body>
+    <?php
+    set_time_limit(10);
+    $direccion = "172.16.28.2";
+    $puerto = "23";
+    $tiempo_colapsa = 120;
+    $password_telnet = "mundo16--";
+    $comandos = "enable";
+    $comandos2 = "config";
+    $comando3 = "interface gpon 0/3";
+    $comando5 = "display ont info 0 all";
+    $comando4 = "quit";
+    $usuario = "edmundom";
 
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-      <!-- Messages Dropdown Menu -->
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li>
-    </ul>
-  </nav>
-  <!-- /.navbar -->
+    
+    $salida = "$usuario\n";
+    $salida .= "$password_telnet\n";
+    $salida .= "enable\n";
+    $salida .= "config\n";
+    $salida .= "interface gpon 0/3\n";
+    $salida .= "display ont autofind 5\n";
+    $salida .= "quit\n";
+    $salida .= "quit\n";
+    $salida .= "quit\n";
+    $salida .= "y\n";
 
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="./" class="brand-link">
-      <img src="./dist/img/Logosinfondo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-5" style="opacity: .8">
-      <span class="brand-text font-weight-light">Emenet Comunica...</span>
-    </a>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="dist/img/profile-user.png" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">Usuario Emenet</a>
-        </div>
-      </div>
+    /*$direccion = "192.168.70.254";
+    $puerto = "23";
+    $tiempo_colapsa = 1200;
+    $password_telnet = "mnet2020";
+    $comandos = "queue";
+    $usuario = "mnet";
+    $salida = "$usuario \r\n";
+    $salida .= "$password_telnet \r\n";*/
 
-      <!-- SidebarSearch Form -->
-      <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Buscar" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-              <li class="nav-item menu-open">
-                <a href="../index.html" class="nav-link active">
-                  <i class="nav-icon fas fa-tachometer-alt"></i>
-                  <i class="fa-solid "></i>
-                  <p>
-                    Dashboard
-                    <i class="right fas fa-angle-left"></i>
-                  </p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="#" class="nav-link active">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Administración</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="nav-icon fas fa-solid fa-users"></i>
-                  <p>
-                    Clientes
-                    <i class="right fas fa-angle-left"></i>
-                  </p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="./pages/Clientes.php" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Clientes</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>
-                        Clientes Router
-                        <i class="right fas fa-angle-left"></i>
-                      </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                      <li class="nav-item">
-                        <a href="#" class="nav-link">
-                          <i class="far fa-dot-circle nav-icon"></i>
-                          <p>Coyoltepec</p>
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link" data-toggle="modal" data-target="#IntFecha">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Reporte Ventas</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="./pages/OrdenesInstalacion.php" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Ordenes Instalación</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-
-              <li class="nav-item">
-                <a href="../index.html" class="nav-link">
-                  <i class="nav-icon fas fa-chart-line"></i>
-                  <p>
-                    Cobranza
-                    <i class="right fas fa-angle-left"></i>
-                  </p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="./pages/PagosBanco.php" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Pagos Banco</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Pagos Factura</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-
-              <li class="nav-item">
-                <a href="../index.html" class="nav-link">
-                  <i class="nav-icon fas fa-sitemap"></i>
-                  <p>
-                    Sistema
-                    <i class="right fas fa-angle-left"></i>
-                  </p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="./pages/routers.php" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Router</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Corte</p>
-                      <i class="right fas fa-angle-left"></i>
-                    </a>
-                    <ul class="nav nav-treeview">
-                      <li class="nav-item">
-                        <a href="#" class="nav-link">
-                          <i class="far fa-dot-circle nav-icon"></i>
-                          <p>Coyoltepec</p>
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Puntos de Acceso</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Planes de internet</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Zonas</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-
-              <li class="nav-item">
-                <a href="../index.html" class="nav-link">
-                  <i class="nav-icon fas fa-wifi"></i>
-                  <p>
-                    Hostpot
-                    <i class="right fas fa-angle-left"></i>
-                  </p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Router</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Lista Planes</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Crear Fichas</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-
-              <li class="nav-item">
-                <a href="../index.html" class="nav-link">
-                  <i class="nav-icon fas fa-cloud"></i>
-                  <p>
-                    AdminOLT
-                    <i class="right fas fa-angle-left"></i>
-                  </p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Proximamente...</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Proximamente...</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Proximamente...</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-
-              <li class="nav-item">
-                <a href="../index.html" class="nav-link">
-                  <i class="nav-icon fas fa-archive"></i>
-                  <p>
-                    Almacen
-                    <i class="right fas fa-angle-left"></i>
-                  </p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Proximamente...</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Proximamente...</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Proximamente...</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Starter Page</h1>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <div class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <!-- /.col-md-6 -->
-          
-          <!-- /.col-md-6 -->
-        </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
-  <div class="modal fade bs-example-modal-sm" id="IntFecha" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-sm" role="document">
-        <form name="PasarFecha" action="./pages/ReportePagos.php" method="POST">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Fecha Inicio</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <?php
-                    $FechaActual = date("Y-m-01");
-                    $fechaAnterior = date("Y-m-d",strtotime($FechaActual."- 6 month"));
-                    ?>
-                    <input class="form-control" type="date" name="FechaRep" value="<?=$fechaAnterior?>">
-                    <div class="mt-3" id='respuesta1'>
-                      <!--Muestra Cliente-->          
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                    <button type="submit" class="btn btn-primary">Generar</button>
-                </div>
-            </div>
-        </form>
-    </div>
-</div>
-  
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-    <div class="p-3">
-      <h5>Title</h5>
-      <p>Sidebar content</p>
-      <p>Sidebar content</p>
-      <p>Sidebar content</p>
-      <p>Sidebar content</p>
-      <p>Sidebar content</p>
-      <p>Sidebar content</p>
-    </div>
-  </aside>
-  <!-- /.control-sidebar -->
-
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      ...
-    </div>
-    <!-- Default to the left -->
-    <strong>Emenet Comunicaciones <a href="https://m-net.mx"> m-net.mx</a>.</strong>
-  </footer>
-</div>
-<!-- ./wrapper -->
-
-<!-- REQUIRED SCRIPTS -->
-
-<!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+    $conecta_telnet = fsockopen($direccion, $puerto, $errno, $errstr, $tiempo_colapsa);
+    if (!$conecta_telnet) {
+        echo "$errstr ($errno)";
+        echo "<br>";
+    } else {
+        echo "&iexcl;Conectado satisfactoriamente!<br>";
+        
+        while (!feof($conecta_telnet)) {
+            fwrite($conecta_telnet, $salida);
+            echo fgets($conecta_telnet, 128).'<br>';
+        } 
+        fclose($conecta_telnet);
+    }
+    ?>
 </body>
+
 </html>
